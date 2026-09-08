885b296086639a786bc33095f3a73e453a5a4d05
madlp24 <mdelapavalondono@gmail.com>
2026-09-08 11:35:55 -0500

    feat(quotes): write a dish for this quote only #106

--- quotes/urls.py
+++ quotes/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("<int:pk>/charges/", views.quote_add_charge, name="quote_add_charge"),
     path("<int:pk>/services/", views.quote_add_service, name="quote_add_service"),
     path("<int:pk>/dishes/", views.quote_add_dish, name="quote_add_dish"),
+    path("<int:pk>/dishes/custom/", views.quote_add_custom_dish, name="quote_add_custom_dish"),
     path("<int:pk>/lines/", views.quote_update_lines, name="quote_update_lines"),
     path("<int:pk>/lines/<int:line_id>/remove/", views.quote_remove_line, name="quote_remove_line"),
 ]
